ea81af194cfc124a251d859bfce20cbd49753de1
Luis Moreno <luismoreno@Luiss-MacBook-Air.local>
2025-05-09 15:23:32 -0700

    st.set moved to below from db import

--- pages/pulltag_upload.py
+++ pages/pulltag_upload.py
@@ -1,8 +1,8 @@
 import streamlit as st
+st.set_page_config(page_title="Bulk Pull-tag TXT Upload", layout="wide")
 from db import get_db_cursor
 
-st.set_page_config(page_title="Bulk Pull-tag TXT Upload", layout="wide")
-st.title("📂 Bulk Pull-tag TXT Upload")
+st.title("📂 Pull-tag TXT Upload")
 
 # File uploader for multiple .txt files
 uploaded_files = st.file_uploader(
