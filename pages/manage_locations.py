49aee5e4e3e4f012b80d4edd9cfdd76087478321
luis7fc <luis7fc@icloud.com>
2025-05-24 13:43:59 -0700

    Update manage_locations.py

--- pages/manage_locations.py
+++ pages/manage_locations.py
@@ -4,14 +4,6 @@ from db import get_db_cursor, get_all_locations
 
 
 def run():
-    st.markdown(
-        """
-        <style>
-        [data-testid="stSidebarNav"] { display: none; }
-        </style>
-        """,
-        unsafe_allow_html=True,
-    )
 
     st.header("Manage Warehouse Locations")
 
